3f6ddbb2b9b3bb999f295293b517ae254237ee92
Howon Lee <howon@howonlee.com>
2014-12-12 09:33:41 -0800

    animations are hard

--- mat.py
+++ mat.py
@@ -1,6 +1,13 @@
 from nltk.corpus import brown
+import numpy as np
+import scipy.sparse as sci_sp
 
 def find_ngrams(input_list, n):
     return zip(*[input_list[i:] for i in range(n)])
 
-print find_ngrams(brown.words(), 2)
+def ngram_iter(input_list, n):
+    some yielding business
+
+if __name__ == "__main__":
+    create the sparse matrix
+    but temporally...
